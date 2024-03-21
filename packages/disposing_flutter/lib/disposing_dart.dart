aea52a6e41ab551843391fcaa5e6b9bfede85529
Altynbek Aidarbekov <altynbek.ktl@gmail.com>
2024-03-22 00:03:51 +0500

    chore: move files for structure

--- packages/disposing_flutter/lib/disposing_dart.dart
+++ packages/disposing_flutter/lib/disposing_dart.dart
@@ -1,8 +1,8 @@
-export 'src/disposable_dart/disposables/disposables/disposables/async_disposables.dart';
-export 'src/disposable_dart/disposables/disposables/disposables/sync_disposables.dart';
-export 'src/disposable_dart/disposables/disposables/disposables/disposable_bag.dart';
-export 'src/disposable_dart/disposables/disposables/disposables/sync_disposable_bag.dart';
-export 'src/disposable_dart/disposables/disposables/disposable.dart';
-export 'src/disposable_dart/disposables/disposables/extensions.dart';
-export 'src/disposable_dart/disposables/disposables/using.dart';
-export 'src/disposable_dart/disposables/disposables/exceptions.dart';
+export 'src/disposable_dart/disposables/async_disposables.dart';
+export 'src/disposable_dart/disposables/sync_disposables.dart';
+export 'src/disposable_dart/disposables/disposable_bag.dart';
+export 'src/disposable_dart/disposables/sync_disposable_bag.dart';
+export 'src/disposable_dart/core/disposable.dart';
+export 'src/disposable_dart/core/extensions.dart';
+export 'src/disposable_dart/core/using.dart';
+export 'src/disposable_dart/core/exceptions.dart';
--- packages/disposing_flutter/lib/disposing_flutter.dart
+++ packages/disposing_flutter/lib/disposing_flutter.dart
@@ -1,2 +1,2 @@
-export './src/disposable_bag_state_mixin.dart';
-export './src/extensions.dart';
+export 'src/disposable_flutter/disposable_bag_state_mixin.dart';
+export 'src/disposable_flutter/extensions.dart';
